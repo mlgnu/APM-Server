002571603c2b7cc8cd8ef1f6024a87eeb7b2f7d5
mlgnu <mohamedtahahelmy@gmail.com>
2024-06-03 15:28:45 +0800

    configure database deployment

--- typeorm.config.ts
+++ typeorm.config.ts
@@ -10,12 +10,12 @@ export class TypeOrmConfigService {
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'postgres',
-      url: 'postgres://mohamed:iy2wxALyi0ez5tBCMprOQunFOAa8c7rZ@dpg-cpddadn109ks73ecoq0g-a.singapore-postgres.render.com/apm',
-      host: this.configService.get<string>('DB_HOST', 'localhost'),
-      port: this.configService.get<number>('DB_PORT', 5432),
-      username: this.configService.get<string>('DB_USER', 'mohamed'),
-      password: this.configService.get<string>('DB_PASSWORD', ''),
-      database: this.configService.get<string>('DB_NAME', 'postgres'),
+      url: this.configService.get<string>('DATABASE_URL'),
+      // host: this.configService.get<string>('DB_HOST', 'localhost'),
+      // port: this.configService.get<number>('DB_PORT', 5432),
+      // username: this.configService.get<string>('DB_USER', 'mohamed'),
+      // password: this.configService.get<string>('DB_PASSWORD', ''),
+      // database: this.configService.get<string>('DB_NAME', 'postgres'),
       entities: entities,
       synchronize: this.configService.get<boolean>('DB_SYNCHRONIZE', true),
       ssl: {
