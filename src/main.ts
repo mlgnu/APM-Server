f66de6d0ce91d75dfc01338093fb10c070ef8173
mlgnu <mohamedtahahelmy@gmail.com>
2024-06-04 09:15:55 +0800

    configure for proxies

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,10 @@ import { SessionEntity } from './typeorm/Session';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const sessionRepo = app.get(DataSource).getRepository(SessionEntity);
   const typeormStore = new TypeormStore({ cleanupLimit: 10 }).connect(
@@ -26,7 +27,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
-  app.use('trust proxy', 1);
+  app.set('trust proxy', 1);
   app.use(
     session({
       name: 'SESSION_ID',
